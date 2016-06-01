ff610bc632c8f52ed5811dea8f6bd547cf8b0070
lx5491 <lx5491@gmail.com>
2016-06-01 17:28:44 -0400

    make max_depth 5

--- svm/metric_svm.py
+++ svm/metric_svm.py
@@ -122,7 +122,7 @@ if __name__ == "__main__":
     # clf = SVC(C=0.1, gamma=0.005, class_weight='balanced')
     # clf = LogisticRegression(C=1.0, class_weight='balanced', dual=False, fit_intercept=True,
     #       intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
-    clf = DecisionTreeClassifier(criterion='entropy')
+    clf = DecisionTreeClassifier(criterion='entropy', max_depth=5)
     clf = clf.fit(X_train, y_train)
     y_predict_train = clf.predict(X_train)
     y_predict = clf.predict(X_test)
