372900ba7b5b728247e661f70ad1c9b5674edeba
lx5491 <lx5491@gmail.com>
2016-06-01 14:52:29 -0400

    make things more structured

--- svm/doc2vec_reply/see_email_data.py
+++ svm/doc2vec_reply/see_email_data.py
@@ -19,13 +19,13 @@ data_filename = sys.argv[1]
 view.load_data(data_filename)
 num_yes_emails = 0
 num_no_emails = 0
-for email in view.emails
-    if email["got_reply"] == "yes" and num_yes_emails < 5:
+for email in view.emails:
+    if email["got_reply"] == "yes" and num_yes_emails < 10:
         print "YES:"
         print email['body'].encode("utf-8")
         print ""
         num_yes_emails += 1
-    elif num_no_emails < 5:
+    elif num_no_emails < 10:
         print "NO:"
         print email['body'].encode("utf-8")
         print ""
