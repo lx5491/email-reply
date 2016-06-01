7e75dadd9d0b01547c70c52e8c7e22e0fe5aa3a9
lx5491 <lx5491@gmail.com>
2016-06-01 15:26:36 -0400

    changed svm to log regression

--- svm/metric_svm.py
+++ svm/metric_svm.py
@@ -116,12 +116,14 @@ if __name__ == "__main__":
     t0 = time()
     param_grid = {'C': [1, 2, 5],
               'gamma': [0.1, 0.5, 1]}
-    clf = GridSearchCV(SVC(kernel='rbf'), param_grid)
+    # clf = GridSearchCV(SVC(kernel='rbf'), param_grid)
     # clf = SVC(C=0.1, gamma=0.005, class_weight='balanced')
+    clf = LogisticRegression(C=1.0, class_weight='balanced', dual=False, fit_intercept=True,
+          intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
     clf = clf.fit(X_train, y_train)
     y_predict_train = clf.predict(X_train)
     y_predict = clf.predict(X_test)
-    print "Best estimator:", clf.best_estimator_
+    # print "Best estimator:", clf.best_estimator_
     print "Done in %0.3f secs" % (time() - t0)
 
     # print "y_train:", y_train
